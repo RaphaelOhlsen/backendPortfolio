3fab1a8b10f1eb939aff8c9f88622cae20d6f313
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2020-02-05 10:39:43 -0300

    Exportando e importando Javascript

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado com JS puro e CSS Animation",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "HN1UjzRSdBk",
+    title: "Desvendando o CSS Grid na prática",
+    duration: "36 min",
+    price: "Free"
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "Array: Higher Order Functions",
+    duration: "54 min",
+    price: "Free"
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "O que é API? REST e RESTful?",
+    duration: "33 min",
+    price: "Free"
+  },
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a variável this no Javascript",
+    duration: "48 min",
+    price: "Free"
+  },
+  {
+    id: "2alg7MQ6_sI",
+    title: "Como usar Git e Github na prática: Guia para iniciantes",
+    duration: "34 min",
+    price: "Free"
+  }
+]
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+const videos = require('./data');
+
+server.use(express.static('public'));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server
+});
+
+server.get("/", (req,res) => {
+  return res.render("about");
+});
+
+server.get("/portfolio", (req,res) => {
+  return res.render("portfolio");
+});
+
+
+server.listen(5000, () => {
+  console.log('Server is running')
+});
